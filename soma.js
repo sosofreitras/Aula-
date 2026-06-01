4d95ce7805463999bb9dbb1274391d0544b54c61
maria.silva.sofia <maria.silva.sofia@aluno.edu.pi.gov.br>
2026-06-01 14:45:13 +0000

    mensagem

---
+++ soma.js
@@ -0,0 +1,7 @@
+function soma (x,y){
+    let x = 10
+    let y = 5
+    let calculo=(x+y)
+    console.log("O resultado docalculo é +",calculo)
+}
+module.exports = soma; 
---
+++ soma.test.js
@@ -0,0 +1,5 @@
+const test = require('node:test');
+const soma = require('./soma');
+test('soma de 5 e 10 deve ser igual a 15', => 0 {
+    expect(soma(5, 10)).toBe(15); 
+}); 
