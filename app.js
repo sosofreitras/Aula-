199fb0bf3902f9bf21dec533e6ee6672ad4d1036
maria.silva.sofia <maria.silva.sofia@aluno.edu.pi.gov.br>
2026-06-17 12:59:31 +0000

    calculadora

---
+++ app.js
@@ -0,0 +1,13 @@
+const calculadora = require('./calculadora.js')
+
+let resultadoSoma = calculadora.soma(1,2);
+console.log(resultadoSoma);
+
+let resultadoSubtracao = calculadora.subtracao(1,2);
+console.log(resultadoSubtracao);
+
+let resultadoMultiplicacao = calculadora.multiplicacao(1,2);
+console.log(resultadoMultiplicacao);
+
+let resultadoDivisao = calculadora.divisao(1,2);
+console.log(resultadoDivisao);
---
+++ calculadora.js
@@ -0,0 +1,22 @@
+function soma (x,y){
+    let calculo=(x+y)
+    console.log("O resultado do calculo é :",calculo)
+}
+function subtracao (x,y){
+    let calculo=(x-y)
+    console.log("O resultado do calculo é :",calculo)
+}
+function multiplicacao (x,y){
+    let calculo=(x*y)
+    console.log("O resultado do calculo é :",calculo)
+}
+function divisao (x,y){
+    let calculo=(x/y)
+    console.log("O resultado do calculo é :",calculo)
+}
+module.exports={
+    soma:soma,
+    subtracao:subtracao,
+    multiplicacao:multiplicacao,
+    divisao:divisao,
+}
