ce8c5cf575dd9a5f55de313225ac0c45b48237ea
maria.silva.sofia <maria.silva.sofia@aluno.edu.pi.gov.br>
2026-06-22 15:17:40 +0000

    produto

---
+++ produto.js
@@ -0,0 +1,8 @@
+const produto = {
+    nome: "Notebook Gamer",
+    preco: 4500.00, 
+    emEstoque: true,
+    }
+    console.log(produto.nome);
+    console.log(produto.preco);
+    console.log(produto.emEstoque);
