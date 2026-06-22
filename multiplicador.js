003dd2dc2805ab2702bf5612b3b07e3a5dffcce4
maria.silva.sofia <maria.silva.sofia@aluno.edu.pi.gov.br>
2026-06-22 13:14:44 +0000

    obj

---
+++ multiplicador.js
@@ -0,0 +1,4 @@
+function multiplicar (x,y){
+    let calculo=(x*y)
+    console.log("O resultado do calculo é :",calculo)
+}
---
+++ objeto.js
@@ -0,0 +1,8 @@
+const meuObjeto = { 
+  nome: "JavaScript",
+  versao: 2026,
+  ativo: true
+  };
+  console.log(meuObjeto.nome);
+  console.log(meuObjeto.versao);
+  console.log(meuObjeto.ativo);
